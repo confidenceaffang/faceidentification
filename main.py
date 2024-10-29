940610dcfe36625f7de3d53617c93cfe670ec854
confidenceaffang <coaffa01@wsc.edu>
2024-10-29 18:53:37 -0500

    added the image and tested it

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import cv2
+import os
 
 alg = "haarcascade_frontalface_default.xml"
+name = cv2.CascadeClassifier(alg)
+
+test_image = "image.png"
+img = cv2.imread(test_image, 0)
+
+# Ensure the directory exists
+if not os.path.exists("stored-faces"):
+    os.makedirs("stored-faces")
+
+faces = name.detectMultiScale(
+    img, scaleFactor=1.05, minNeighbors=5, minSize=(100, 100)
+)
+
+j = 0
+
+for x, y, w, h in faces:
+    cropped_image = img[y:y+h, x:x+w]
+    target_file = "stored-faces/" + str(j) + '.jpg'
+    cv2.imwrite(target_file, cropped_image)
+    j += 1
